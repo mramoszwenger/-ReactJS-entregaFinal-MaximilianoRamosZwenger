fe4f6400c21e083f7f657aa74464acfb7e1b09bf
mramoszwenger <mramoszwenger@gmail.com>
2024-01-22 18:11:56 -0300

    preEntrega1-MRZ-v1.1

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,30 +1,45 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import CartWidget from "../CartWidget/CartWidget";
 
 const NavBar = () => {
     return(
-        <>
+      <>
+        <Navbar expand="lg" bg="light" id="principal-nav">
+          <Container>
+            <Navbar.Brand href="#" className="animate__animated animate__bouncein">
+              <img src="/logotipoEmpresa.png" alt="logo-empresa" className="company__logo"/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="mainNav" />
+            <Navbar.Collapse id="mainNav">
+              <Nav className="ms-auto">
+                <Nav.Item>
+                  <Nav.Link href="#">Inicio</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="#nosotros">Nosotros</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <NavDropdown title="Servicios" id="navbarDropdown" align="end">
+                    <NavDropdown.Item href="#servicios">Administración de Sistemas</NavDropdown.Item>
+                    <NavDropdown.Item href="#servicios">Redes de Datos y Telecomunicaciones</NavDropdown.Item>
+                    <NavDropdown.Item href="#servicios">Soluciones en la Nube</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="contacto.html">Contacto</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
 
-            <h1>LOGO</h1>
+        <CartWidget/>
 
-            <ul>
-                <li>
-                  <a>Inicio</a>
-                </li>
-                <li>
-                  <a>Nosotros</a>
-                </li>
-                <li>
-                  <a>Servicios</a>
-                </li>
-                <li>
-                  <a>Contacto</a>
-                </li>
-            </ul>
-
-            <CartWidget/>
-
-        </>
+      </>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
