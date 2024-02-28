3f9ed1e53655ace9190d91ee70e143febc03e2b9
mramoszwenger <mramoszwenger@gmail.com>
2024-02-28 20:40:52 -0300

    entregaFinal-MRZ-v3.1

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContex } from '../../context/CartContex';
 
 const Cart = () => {
+
+    const {cart} = useContext(CartContex)
+
   return (
-    <div>Cart</div>
+    <div>
+        <h3>Carrito</h3>
+        {cart.length == 0 ? <>
+        <h4>No hay servicios agregados</h4>
+        </> : <h4>Lista de servicios</h4>
+        }
+    </div>
   )
 }
 
