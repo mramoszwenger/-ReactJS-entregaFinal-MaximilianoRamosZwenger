372324e421ba61519b84de8525c083c334101c31
mramoszwenger <mramoszwenger@gmail.com>
2024-02-29 21:07:52 -0300

    entregaFinal-MRZ-v3.3

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,28 +2,36 @@ import React, { useContext } from 'react';
 import { CartContex } from '../../context/CartContex';
 import { Link } from 'react-router-dom';
 import CartItem from '../CartItem/CartItem';
+import './Cart.css';
 
 const Cart = () => {
 
     const {cart,removeService,emptyCart,totalServices} = useContext(CartContex)
 
   return (
-    <div>
-        {cart.length == 0 ? <>
-        <h4>No hay servicios agregados</h4>
-        <Link to={"/"}><button>Volver al Inicio</button></Link>
-        </> : 
-              <>
-                <h4>Servicios a Solicitar</h4>
-                {cart.map((s) => (
-                  <CartItem key={s.id} servicio={s} removeService={removeService}/>
-                ))}
+    <div className="cart__itemsContainer">
+        <div className="cart__items">
+            {cart.length === 0 ? (
+                <>
+                    <h4>No hay servicios agregados</h4>
+                    <Link to={"/"}><button>Volver al Inicio</button></Link>
+                </>
+            ) : (
+                <>
+                  {cart.map((s) => (
+                      <CartItem key={s.servicio.id} servicio={s} removeService={removeService} />
+                  ))}
+                </>
+            )}
+        </div>
 
-                <h4>Total: ${totalServices()}</h4>
-
-                <button onClick={emptyCart}>Vaciar Carrito</button>
-              </>
-        }
+        {cart.length > 0 && (
+            <div className="cart__summaryContainer">
+                <h3>Resumen Servicios a Contratar</h3>
+                <h4 className='cart__summaryTotal'>Costo estimado: ${totalServices()}</h4>
+                <button className='cart__summaryButton' onClick={emptyCart}>Vaciar Carrito</button>
+            </div>
+        )}
     </div>
   )
 }
--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import './CartItem.css';
 
 const CartItem = ({servicio,removeService}) => {
 
     return (
-        <div>
-            <h3>{servicio.servicio.nombre}</h3>
-            <img src={servicio.servicio.img} alt={servicio.servicio.nombre}/>
-            <p>Cantidad: {servicio.cantidad}</p>
-            <p>Costo estimado: ${servicio.servicio.precio*servicio.cantidad}</p>
-            <button onClick={()=> removeService(servicio.servicio.id)}>Eliminar servicio</button>
+        <div className="cartItem__container">
+            <div className="cartItem__imgContainer">
+                <img src={servicio.servicio.img} alt={servicio.servicio.nombre} className="cartItem__image" />
+            </div>
+            <div className="cartItem__descriptionContainer">
+                <h3 className="cartItem__title">{servicio.servicio.nombre}</h3>
+                <p className="cartItem__quantity">Cantidad: {servicio.cantidad}</p>
+                <p className="cartItem__price">Costo estimado: ${servicio.servicio.precio * servicio.cantidad}</p>
+                <button className="cartItem__button" onClick={() => removeService(servicio.servicio.id)}>
+                    Eliminar servicio
+                </button>
+            </div>
         </div>
     );
 };
