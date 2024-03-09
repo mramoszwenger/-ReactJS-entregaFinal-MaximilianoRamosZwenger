1afce5affc915698729bce2de0e2e23f34f1e9a4
mramoszwenger <mramoszwenger@gmail.com>
2024-03-09 20:28:16 -0300

    entregaFinal-MRZ-v3.3.8

--- src/components/CheckOut/CheckOut.jsx
+++ src/components/CheckOut/CheckOut.jsx
@@ -3,14 +3,14 @@ import { db } from '../../firebase/config';
 import { collection, addDoc, doc } from 'firebase/firestore';
 import { CartContex } from '../../context/CartContex';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faEnvelope, faPhone, faBriefcase } from '@fortawesome/free-solid-svg-icons';
 import './CheckOut.css';
 
 const CheckOut = () => {
     const { cart, emptyCart, totalServices } = useContext(CartContex);
 
     const [name, setName] = useState('');
-    const [surname, setSurname] = useState('');
+    const [company, setCompany] = useState('');
     const [phone, setPhone] = useState('');
     const [email, setEmail] = useState('');
     const [emailValidation, setEmailValidation] = useState('');
@@ -20,8 +20,8 @@ const CheckOut = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if (!name || !surname || !phone || !email || !emailValidation) {
-            setError('Completar los campos requeridos');
+        if (!name || !company || !phone || !email || !emailValidation) {
+            setError('Todos los campos son requeridos para poder procesar la solicitud');
             return;
         }
 
@@ -39,7 +39,7 @@ const CheckOut = () => {
             total: totalServices(),
             fecha: new Date(),
             name,
-            surname,
+            company,
             phone,
             email,
         };
@@ -47,7 +47,6 @@ const CheckOut = () => {
         Promise.all(
             orden.items.map(async (serviceOrder) => {
                 const serviceRef = doc(db, 'item', serviceOrder.id);
-                // Aquí podrías realizar alguna acción con serviceRef si es necesario
             })
         )
             .then(() => {
@@ -70,7 +69,7 @@ const CheckOut = () => {
                 <h3 className="mb-4 checkout__title">Ingresa tus datos para completar la solicitud</h3>
 
                 <form onSubmit={handleSubmit} className="mb-4 checkout__form">
-                    <ul>
+                    <ul className='summary__checkout'>
                         {cart.map((s) => (
                             <li key={s.servicio.id} className="list__summaryServices">
                                 {s.servicio.nombre} ({s.cantidad})
@@ -79,78 +78,53 @@ const CheckOut = () => {
                     </ul>
 
                     <div className="form__group">
-                        <div className="input-with-icon">
-                            <label htmlFor="name" className="visually-hidden">
-                                Nombre
-                            </label>
-                            <div className="input-container">
-                                <FontAwesomeIcon icon={faUser} className="input-icon" />
-                                <input
-                                    name="name"
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Escribe tu nombre"
-                                    onChange={(e) => setName(e.target.value)}
-                                />
+
+                        <div>
+                            <label htmlFor="name" className="form__label">Nombre y Apellido</label>
+                            <div className="input__container">
+                                <FontAwesomeIcon icon={faUser} className="input__icon"/>
+                                <input name="name" type="text" className="input__form" placeholder="Tu Nombre y Apellido" onChange={(e) => setName(e.target.value)}/>
                             </div>
                         </div>
 
                         <div>
-                            <label htmlFor="surname">Apellido</label>
-                            <input
-                                name="surname"
-                                type="text"
-                                className="form-control"
-                                placeholder="Apellido"
-                                onChange={(e) => setSurname(e.target.value)}
-                            />
+                            <label htmlFor="company" className="form__label">Empresa</label>
+                            <div className="input__container">
+                                <FontAwesomeIcon icon={faBriefcase} className="input__icon"/>
+                                <input name="company" type="text" className="input__form" placeholder="Nombre de la Empresa" onChange={(e) => setCompany(e.target.value)}/>
+                            </div>
                         </div>
 
                         <div>
-                            <label htmlFor="phone">
-                                <FontAwesomeIcon icon={faPhone} /> Teléfono
-                            </label>
-                            <input
-                                name="phone"
-                                type="text"
-                                className="form-control"
-                                placeholder="Teléfono"
-                                onChange={(e) => setPhone(e.target.value)}
-                            />
+                            <label htmlFor="phone" className="form__label">Teléfono</label>
+                            <div className="input__container">
+                                <FontAwesomeIcon icon={faPhone} className="input__icon"/>
+                                <input name="phone" type="text" className="input__form" placeholder="Un Número de Contacto" onChange={(e) => setPhone (e.target.value)}/>
+                            </div>
                         </div>
 
                         <div>
-                            <label htmlFor="email">
-                                <FontAwesomeIcon icon={faEnvelope} /> Email
-                            </label>
-                            <input
-                                name="email"
-                                type="email"
-                                className="form-control"
-                                placeholder="Correo Electrónico"
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
+                            <label htmlFor="email" className="form__label">Correo</label>
+                            <div className="input__container">
+                                <FontAwesomeIcon icon={faEnvelope} className="input__icon"/>
+                                <input name="email" type="email" className="input__form" placeholder="Un Correo Electrónico" onChange={(e) => setEmail (e.target.value)}/>
+                            </div>
                         </div>
 
                         <div>
-                            <label htmlFor="emailValidation">Email Confirmación</label>
-                            <input
-                                name="emailValidation"
-                                type="email"
-                                className="form-control"
-                                placeholder="Confirmar Correo Electrónico"
-                                onChange={(e) => setEmailValidation(e.target.value)}
-                            />
+                            <label htmlFor="emailValidation" className="form__label">Correo</label>
+                            <div className="input__container">
+                                <FontAwesomeIcon icon={faEnvelope} className="input__icon"/>
+                                <input name="emailValidation" type="email" className="input__form" placeholder="Confirma el Correo Electrónico" onChange={(e) => setEmailValidation (e.target.value)}/>
+                            </div>
                         </div>
 
                         <button type="submit" className="btn btn-primary mt-3 checkout__button">Solicitar Servicio/s</button>
 
-                        {error && <p style={{ color: 'red' }}>{error}</p>}
+                        {error && <p style={{ color: 'red' }} className='error__message'>{error}</p>}
 
                         {orderId && (
-                            <p>
-                                ¡Gracias por confiar en nosotros! Tu número de control es: {orderId}
-                            </p>
+                            <p className='confirmation__message'>¡Gracias por confiar en nosotros! Tu número de control es: {orderId}</p>
                         )}
                     </div>
                 </form>
