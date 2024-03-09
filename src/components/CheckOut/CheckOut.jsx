c7742963dafd634c61fe086a98997c183d2e7df2
mramoszwenger <mramoszwenger@gmail.com>
2024-03-09 00:32:06 -0300

    entregaFinal-MRZ-v3.3.7

--- src/components/CheckOut/CheckOut.jsx
+++ src/components/CheckOut/CheckOut.jsx
@@ -1,30 +1,32 @@
-import React, { useState,useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { db } from '../../firebase/config';
-import { collection,addDoc,updateDoc,doc,getDoc } from 'firebase/firestore';
+import { collection, addDoc, doc } from 'firebase/firestore';
 import { CartContex } from '../../context/CartContex';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import './CheckOut.css';
 
 const CheckOut = () => {
+    const { cart, emptyCart, totalServices } = useContext(CartContex);
 
-    const {cart,emptyCart,cartCount,totalServices} = useContext(CartContex)
-
-    const [name,setName] = useState("")
-    const [surname,setSurname] = useState("")
-    const [phone,setPhone] = useState("")
-    const [email,setEmail] = useState("")
-    const [emailValidation,setEmailValidation] = useState("")
-    const [error,setError] = useState("")
-    const [orderId,setOrderId] = useState("")
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [emailValidation, setEmailValidation] = useState('');
+    const [error, setError] = useState('');
+    const [orderId, setOrderId] = useState('');
 
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
 
-        if(!name || !surname || !phone || !email || !emailValidation){
-            setError("Completar los campos requeridos")
+        if (!name || !surname || !phone || !email || !emailValidation) {
+            setError('Completar los campos requeridos');
             return;
         }
 
-        if(email !== emailValidation) {
-            setError("El Correo Electrónico no coincide")
+        if (email !== emailValidation) {
+            setError('El Correo Electrónico no coincide');
             return;
         }
 
@@ -32,86 +34,129 @@ const CheckOut = () => {
             items: cart.map((s) => ({
                 id: s.servicio.id,
                 nombre: s.servicio.nombre,
-                cantidad: s.cantidad
+                cantidad: s.cantidad,
             })),
             total: totalServices(),
             fecha: new Date(),
             name,
             surname,
             phone,
-            email
-        }
+            email,
+        };
 
         Promise.all(
             orden.items.map(async (serviceOrder) => {
-                const serviceRef = doc(db,"item",serviceOrder.id);
+                const serviceRef = doc(db, 'item', serviceOrder.id);
+                // Aquí podrías realizar alguna acción con serviceRef si es necesario
             })
         )
-        .then(() => {
-            addDoc(collection(db,"ordenes"),orden)
-            .then((docRef) => {
-                setError("")
-                setOrderId(docRef.id)
-                emptyCart()
-            })
-            .catch((error) => {
-                console.log(error)
-                setError("Se produjo un error al crear la orden")
-            })
-        })
-    }
+            .then(() => {
+                addDoc(collection(db, 'ordenes'), orden)
+                    .then((docRef) => {
+                        setError('');
+                        setOrderId(docRef.id);
+                        emptyCart();
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        setError('Se produjo un error al crear la orden');
+                    });
+            });
+    };
 
     return (
-        <div>
+        <div className='checkout__container'>
+            <div className='checkout__formContainer'>
+                <h3 className="mb-4 checkout__title">Ingresa tus datos para completar la solicitud</h3>
 
-            <h3>Ingresa tus datos</h3>
+                <form onSubmit={handleSubmit} className="mb-4 checkout__form">
+                    <ul>
+                        {cart.map((s) => (
+                            <li key={s.servicio.id} className="list__summaryServices">
+                                {s.servicio.nombre} ({s.cantidad})
+                            </li>
+                        ))}
+                    </ul>
 
-            <form onSubmit={handleSubmit}>
+                    <div className="form__group">
+                        <div className="input-with-icon">
+                            <label htmlFor="name" className="visually-hidden">
+                                Nombre
+                            </label>
+                            <div className="input-container">
+                                <FontAwesomeIcon icon={faUser} className="input-icon" />
+                                <input
+                                    name="name"
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Escribe tu nombre"
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                            </div>
+                        </div>
 
-                <ul>
-                    {cart.map((s) => (
-                        <li key={s.servicio.id} className='list__summaryServices'>{s.servicio.nombre} ({s.cantidad})</li>
-                    ))}
-                </ul>
+                        <div>
+                            <label htmlFor="surname">Apellido</label>
+                            <input
+                                name="surname"
+                                type="text"
+                                className="form-control"
+                                placeholder="Apellido"
+                                onChange={(e) => setSurname(e.target.value)}
+                            />
+                        </div>
 
-                <div>
-                    <div>
-                        <label htmlFor="name">Nombre</label>
-                        <input name="name" type='text' placeholder="Nombre" onChange={(e) => setName(e.target.value)}/>
+                        <div>
+                            <label htmlFor="phone">
+                                <FontAwesomeIcon icon={faPhone} /> Teléfono
+                            </label>
+                            <input
+                                name="phone"
+                                type="text"
+                                className="form-control"
+                                placeholder="Teléfono"
+                                onChange={(e) => setPhone(e.target.value)}
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="email">
+                                <FontAwesomeIcon icon={faEnvelope} /> Email
+                            </label>
+                            <input
+                                name="email"
+                                type="email"
+                                className="form-control"
+                                placeholder="Correo Electrónico"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="emailValidation">Email Confirmación</label>
+                            <input
+                                name="emailValidation"
+                                type="email"
+                                className="form-control"
+                                placeholder="Confirmar Correo Electrónico"
+                                onChange={(e) => setEmailValidation(e.target.value)}
+                            />
+                        </div>
+
+                        <button type="submit" className="btn btn-primary mt-3 checkout__button">Solicitar Servicio/s</button>
+
+                        {error && <p style={{ color: 'red' }}>{error}</p>}
+
+                        {orderId && (
+                            <p>
+                                ¡Gracias por confiar en nosotros! Tu número de control es: {orderId}
+                            </p>
+                        )}
                     </div>
-
-                    <div>
-                        <label htmlFor="surname">Apellido</label>
-                        <input name="surname" type='text' placeholder="Apellido" onChange={(e) => setSurname(e.target.value)}/>
-                    </div>
-
-                    <div>
-                        <label htmlFor="phone">Teléfono</label>
-                        <input name="phone" type='text' placeholder="Teléfono" onChange={(e) => setPhone(e.target.value)}/>
-                    </div>
-
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <input name="email" type='email' placeholder="Correo Electrónico" onChange={(e) => setEmail(e.target.value)}/>
-                    </div>
-
-                    <div>
-                        <label htmlFor="emailValidation">Email Confirmacion</label>
-                        <input name="emailValidation" type='email' placeholder="Confirmar Correo Electrónico" onChange={(e) => setEmailValidation(e.target.value)}/>
-                    </div>
-
-                    <button type='submit'>Confirmar compra</button>
-
-                    {error && <p style={{color: "red"}}>{error}</p>}
-
-                    {orderId && (
-                        <p>¡Gracias por confiar en nosotros! Tu número de control es: {orderId}</p>
-                    )}
-                </div>
-
-            </form>
+                </form>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default CheckOut
+export default CheckOut;
